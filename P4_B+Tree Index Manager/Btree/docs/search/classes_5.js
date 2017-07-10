ae55de353556a37f20154dcde23555d581ff37e0
times123 <yangmiao168@gmail.com>
2017-07-09 22:11:22 -0500

    remove docs

--- P4_B+Tree Index Manager/Btree/docs/search/classes_5.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['leafnodeint',['LeafNodeInt',['../structbadgerdb_1_1_leaf_node_int.html',1,'badgerdb']]]
-];
--- P4_B+Tree Index Manager/Btree/docs/search/variables_7.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['level',['level',['../structbadgerdb_1_1_non_leaf_node_int.html#a1409af967028a575417da623134f1f7f',1,'badgerdb::NonLeafNodeInt::level()'],['../structbadgerdb_1_1_leaf_node_int.html#a99b5fd20bafd3b124725536f89e04163',1,'badgerdb::LeafNodeInt::level()']]]
-];
